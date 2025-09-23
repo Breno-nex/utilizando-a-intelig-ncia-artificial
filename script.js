80fce8a30b8ec9f9ac46911f5f6b55f0e6125592
Breno-nex <161513182+Breno-nex@users.noreply.github.com>
2025-09-23 11:17:26 -0300

    23/09/2025

--- script.js
+++ script.js
@@ -6,32 +6,31 @@ const textoResultado=document.querySelector(".texto-resultado");
 
 const perguntas = [
 {
-    enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+    enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as duvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual é o primeiro pensamento",
     alternativas:[
         {
-        texto:"Isso é assustador!",
+        texto:"isso é assustador",
         afirmacao:"afirmação"
         },
         {
-        texto:"Isso é maravilhoso!",
-        afirmacao:"afirmação"
-        },
+       texto:"isso é maravilhoso",
+       afirmacao:"afirmação"
+        }
 
     ]
 },
 
 {
     
-        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial (IA), uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
-        alternativas:[
-            {
+        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial (IA), uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma",
+        alternativas:[{
             texto:"Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
             afirmacao:"afirmação"
-            },
-            {
-            texto:"Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
-            afirmacao:"afirmação"
-            }
+        },
+        {        
+           texto:"Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
+           afirmacao:"afirmação"
+        }        
         ]
     },
     
@@ -42,7 +41,7 @@ const perguntas = [
         alternativas:[
             {
             texto:"Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-            afirmacao:"afirmação"
+            afirmacao:"afrimação"
             },
             {
             texto:"Me preocupo com as pessoas que perderão seus empregos para máquinas e defendo a importância de proteger os trabalhadores.",
@@ -51,8 +50,6 @@ const perguntas = [
         ]
     },
     
-
-
     {
     
         enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
@@ -60,14 +57,13 @@ const perguntas = [
             {
             texto:"Criar uma imagem utilizando uma plataforma de design como o Paint.",
             afirmacao:"afirmação"
-            },
-            {
+        },
+        {
             texto:"Criar uma imagem utilizando um gerador de imagem de IA.",
             afirmacao:"afirmação"
-            }
+        }
         ]
     },
-    
 
     {
     
@@ -77,53 +73,50 @@ const perguntas = [
             texto:"Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
             afirmacao:"afirmação"
             },
-            {
+        {
             texto:"O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
             afirmacao:"afirmação"
-            }
+        }
         ]
     },
-]
+];
 
-let atual =0;
+let atual = 0;
 let perguntaAtual;
-let historiafinal
+let historiaFinal = "";
 
 function mostraPergunta(){
     if (atual >= perguntas.length){
-    mostraResultado();
-    return;
+        mostraResultado();
+        return;
     }
-
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
     caixaAlternativas.textContent = "";
-    mostraAlternativa();
-
-}
-
-function mostraAlternativa(){
-    for(const alternativa of perguntaAtual.alternativa){
+    mostraAlternativas();
+    }
+    
+    function mostraAlternativas(){
+    for(const alternativa of perguntaAtual.alternativas){
     const botaoAlternativas = document.createElement("button");
-    botaoAlternativas.textContent = alternativa;
+    botaoAlternativas.textContent = alternativa.texto;
     botaoAlternativas.addEventListener("click",() => respostaSelecionada(alternativa));
     caixaAlternativas.appendChild(botaoAlternativas);
     }
-}
+     }
 
-function respostaSelecionada(opcaoSelecionada){
-    const afirmacoes = opcaoSelecionada.afirmacas;
-    historiafinal += afirmacoes;
-    atual++;
-    mostraPergunta();
+     function respostaSelecionada(opcaoSelecionada){
+        const afirmacoes = opcaoSelecionada. afirmacoes;
+        historiaFinal += afirmacoes + "" ;
+        atual++;
+        mostraPergunta();
 
-}
+     }
 
-function mostraResultado(){
-    caixaPerguntas.textContent = "Em 2049...";
-    textoResultado.textContent = historiafinal;
-    caixaAlternativas.textContent = "";
-
-}
-
-mostraPergunta();
+     function mostraResultado(){
+        caixaPerguntas.textContent = "Em 2049...";
+        textoResultado.textContent = historiaFinal;
+        caixaAlternativas.textContent = "";
+     }
+    
+     mostraPergunta();
